8a263daf5c6dca8137f595de95ca490a03eca4d6
Николай Секирин <snv031@gmail.com>
2025-05-12 10:27:59 +0300

    Коммент к слипу

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart_button_is_present(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(10)  # слип для проверки языка на кнопке
     add_to_cart_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
 
     assert add_to_cart_button.is_displayed(), "Кнопка добавления в корзину не найдена на странице"
